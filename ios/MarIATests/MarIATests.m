4f3038df772e1138bed744ec8cebddcd77bece5b
Angel Mondragon <damianmor3005@hotmail.com>
2024-09-21 09:08:50 -0600

    cambio nombre

--- ios/MarIATests/MarIATests.m
+++ ios/MarIATests/MarIATests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProject3Tests : XCTestCase
+@interface MarIATests : XCTestCase
 
 @end
 
-@implementation AwesomeProject3Tests
+@implementation MarIATests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
