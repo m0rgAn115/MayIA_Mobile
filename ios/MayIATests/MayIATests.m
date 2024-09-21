8cfa33ca09737925aa3a913a60ceca568c03eb8d
Angel Mondragon <damianmor3005@hotmail.com>
2024-09-21 09:17:34 -0600

    cambio de nombre

--- ios/MayIATests/MayIATests.m
+++ ios/MayIATests/MayIATests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MarIATests : XCTestCase
+@interface MayIATests : XCTestCase
 
 @end
 
-@implementation MarIATests
+@implementation MayIATests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
